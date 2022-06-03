71084a976a129dcd86206bcb59f5c8282f07236c
Mikhail Brinchuk <thecentury@gmail.com>
2022-06-03 22:34:57 +0200

    It looks like the full core is ported

--- src/ConnectFour.purs
+++ src/ConnectFour.purs
@@ -4,12 +4,16 @@ import OwnPrelude
 import Prelude
 
 import Control.Monad.Reader (Reader, ask)
-import Data.Foldable (maximum, minimum)
+import Data.Foldable (foldr, maximum, minimum)
 import Data.FunctorWithIndex (mapWithIndex)
-import Data.List (List, filter, take, transpose, (..), concat, null, fromFoldable)
+import Data.List (List(..), all, concat, filter, fromFoldable, head, null, reverse, take, transpose, (..))
+import Data.List as List
 import Data.Maybe (Maybe(..), fromMaybe)
+import Data.Traversable (sequence)
+import Data.Tuple (Tuple(..))
 import Data.Unfoldable (replicate)
 import Effect.Exception.Unsafe (unsafeThrow)
+import Control.Alt ((<|>))
 
 type Config = {
   rows :: Int,
@@ -48,9 +52,9 @@ instance showPlayer :: Show Player where
 derive instance eqPlayer :: Eq Player
 derive instance ordPlayer :: Ord Player
 
-playerMinimax :: Player -> Array Player -> Player
+playerMinimax :: Player -> List Player -> Player
 playerMinimax B _ = B
-playerMinimax _ [] = B
+playerMinimax _ Nil = B
 playerMinimax O winners = fromMaybe O (minimum winners)
 playerMinimax X winners = fromMaybe X (maximum winners)
 
@@ -62,6 +66,8 @@ nextPlayer X = O
 type BoardRow = List Player
 type Column = List Player
 type Board = List BoardRow
+
+type ColumnNumber = Int
 
 mkBoard :: Config -> Board
 mkBoard cfg = replicate cfg.rows (replicate cfg.columns B)
@@ -89,3 +95,181 @@ boardDiagonals board = do
         # take 1)
       # filter (not <<< null)
       # transpose
+
+winnerInRow :: BoardRow -> Reader Config (Maybe Player)
+winnerInRow row = do
+  cfg <- config
+  let toWin = cfg.win
+  pure $ impl toWin (Tuple B 0) row
+  where
+    impl :: Int -> (Tuple Player Int) -> List Player -> Maybe Player
+    impl toWin (Tuple player count) Nil = if count == toWin then Just player else Nothing
+    impl toWin (Tuple player count) (Cons x xs) = 
+      if x == player then 
+        impl toWin (Tuple player (count + 1)) xs 
+      else 
+        impl toWin (Tuple (nextPlayer player) 0) xs
+
+winner :: Board -> Reader Config (Maybe Player)
+winner board = do
+  diagonals <- boardDiagonals board
+  let toSearch = concat $ fromFoldable [boardRows board, boardColumns board, diagonals]
+  winners <- sequence $ (map winnerInRow) $ toSearch
+  pure $ fromMaybe Nothing (maximum winners)
+
+isFullColumn :: Column -> Boolean
+isFullColumn column = head column /= Just B
+
+isFullBoard :: Board -> Boolean
+isFullBoard board = all isFullColumn (boardColumns board)
+
+tryAddToColumn :: Player -> Column -> Maybe Column
+tryAddToColumn player column = 
+  let (Tuple added result) = foldr go (Tuple false Nil) column
+  in if added then Just result else Nothing
+  where
+    go :: Player -> Tuple Boolean (List Player) -> Tuple Boolean (List Player)
+    go current (Tuple true soFar) = Tuple true (Cons current soFar)
+    go B (Tuple _ soFar) = Tuple true (Cons player soFar)
+    go current (Tuple _ soFar) = Tuple false (Cons current soFar)
+
+tryAddToBoard :: Player -> ColumnNumber -> Board -> Maybe Board
+tryAddToBoard player columnIndex board =
+  map transpose $ go columnIndex Nil $ boardColumns board
+  where
+    go :: ColumnNumber -> List Column -> List Column -> Maybe (List Column)
+    go _ _ Nil = Nothing
+    go 0 soFar (Cons column rest) = 
+      let column' = tryAddToColumn player column in
+      case column' of
+        Nothing -> Nothing
+        Just column'' -> Just $ concat $ List.fromFoldable [reverse soFar, Cons column'' rest]
+    go currentColumnIndex soFar (Cons column rest) =
+      go (currentColumnIndex - 1) (Cons column soFar) rest
+
+nextMoves :: Player -> Board -> List Board
+nextMoves player board = 
+  board
+  # boardColumns
+  # zipperFromList
+  # zipperSelfAndRights
+  # List.mapMaybe (\z -> tryAddToColumn player z.focus # map (\column -> zipperWithFocus column z))
+  # map (boardColumns <<< zipperToList)
+
+--------------------------------------------------------------------------------
+
+data Winner =
+  DepthExhausted
+  | FoundWinner Player
+  | WinnerInChildren Player
+
+derive instance eqWinner :: Eq Winner
+
+winnerToPlayer :: Winner -> Player
+winnerToPlayer (FoundWinner player) = player
+winnerToPlayer (WinnerInChildren player) = player
+winnerToPlayer DepthExhausted = B
+
+type GameTreeNode = {
+  playerToPlay :: Player,
+  board :: Board,
+  winner :: Winner,
+  depth :: Int
+}
+
+buildGameTree :: Player -> Board -> Reader Config (Tree GameTreeNode)
+buildGameTree playerToPlay' board' = do
+  cfg <- config
+  result <- impl cfg.maxDepth 0 playerToPlay' board'
+  pure result
+  where
+    impl :: Int -> Int -> Player -> Board -> Reader Config (Tree GameTreeNode)
+    impl maxDepth currentDepth player board = do
+      if currentDepth >= maxDepth then
+        let treeValue' = {
+          playerToPlay: player,
+          board: board,
+          winner: DepthExhausted,
+          depth: currentDepth
+        }
+        in pure $ Tree treeValue' Nil
+      else do
+        let nextPlayer' = nextPlayer player
+        winner' <- winner board
+        (Tuple actualWinner children) <- do
+          case winner' of
+            Just w -> pure $ Tuple (FoundWinner w) Nil
+            Nothing -> do
+              let nextMoves' = nextMoves nextPlayer' board
+              children <- 
+                nextMoves'
+                # map (impl maxDepth (currentDepth + 1) nextPlayer')
+                # sequence
+              let childrenWinners = map (\tree -> winnerToPlayer (treeValue tree).winner) children
+              let nodeWinner = playerMinimax player childrenWinners
+              pure $ Tuple (WinnerInChildren nodeWinner) children
+        let v = {
+          playerToPlay: player,
+          board: board,
+          winner: actualWinner,
+          depth: currentDepth
+        }
+        pure $ Tree v children
+
+--------------------------------------------------------------------------------
+
+data AIMove =
+  Definite Board
+  | RandomGuess Board
+
+derive instance eqAIMove :: Eq AIMove
+
+nextBoardFromMove :: AIMove -> Board
+nextBoardFromMove (Definite board) = board
+nextBoardFromMove (RandomGuess board) = board
+
+-- todo define nextMove with randomness
+nextMove :: Player -> Board -> Reader Config (Maybe AIMove)
+nextMove currentPlayer board = do
+  tree <- buildGameTree (nextPlayer currentPlayer) board
+  let definiteGuess =
+        tree
+        # treeChildren
+        # filter (\tree' -> winnerToPlayer (treeValue tree').winner == currentPlayer)
+        # head
+        # map (\child -> Definite (treeValue child).board)
+  let candidateOne = definiteGuess
+  let candidateTwo =
+        tree
+        # treeChildren
+        # filter (\tree' -> winnerToPlayer (treeValue tree').winner == B)
+        # map (\child -> (treeValue child).board)
+        # randomMove
+  let candidateThree =
+        tree
+        # treeChildren
+        # map (\child -> (treeValue child).board)
+        # randomMove
+  pure $ candidateOne <|> candidateTwo <|> candidateThree
+
+  where
+    randomMove :: List Board -> Maybe AIMove
+    randomMove Nil = Nothing
+    randomMove (Cons b _) = Just $ RandomGuess b
+
+--------------------------------------------------------------------------------
+
+data Outcome =
+  Win Player
+  | Draw
+  | InProgress
+
+derive instance eqOutcome :: Eq Outcome
+
+boardOutcome :: Board -> Reader Config Outcome
+boardOutcome board = do
+  winner' <- winner board
+  case winner' of
+    Just w -> pure $ Win w
+    Nothing | isFullBoard board -> pure Draw
+    Nothing -> pure InProgress
--- src/OwnPrelude.purs
+++ src/OwnPrelude.purs
@@ -3,8 +3,54 @@ module OwnPrelude where
 import Prelude
 
 import Data.List (List(..), drop)
+import Data.List as List
+import Data.Maybe (Maybe(..))
+import Data.Tuple (Tuple(..))
+import Data.Unfoldable (unfoldr)
+import Effect.Exception.Unsafe (unsafeThrow)
 
 safeSkip :: forall a . Int -> List a -> List a
 safeSkip count _ | count < 0 = Nil
 safeSkip 0 list = list
 safeSkip toSkip list = drop toSkip list
+
+type Zipper a = {
+    left :: List a,
+    focus :: a,
+    right :: List a
+}
+
+zipperFromList :: forall a . List a -> Zipper a
+zipperFromList Nil = unsafeThrow "zipperFromList: empty list"
+zipperFromList (Cons x xs) = { left: Nil, focus: x, right: xs }
+
+zipperWithFocus :: forall a . a -> Zipper a -> Zipper a
+zipperWithFocus f z = { left: z.left, focus: f, right: z.right }
+
+zipperTryMoveRight :: forall a . Zipper a -> Maybe (Zipper a)
+zipperTryMoveRight { left, focus, right } = case right of
+    Nil -> Nothing
+    Cons r rs -> Just { left: Cons focus left, focus: r, right: rs }
+
+zipperSelfAndRights :: forall a . Zipper a -> List (Zipper a)
+zipperSelfAndRights zipper = unfoldr gen (Just zipper)
+  where
+    gen :: Maybe (Zipper a) -> Maybe (Tuple (Zipper a) (Maybe (Zipper a)))
+    gen Nothing = Nothing
+    gen (Just z) = Just $ Tuple z (zipperTryMoveRight z)
+
+zipperToList :: forall a . Zipper a -> List a
+zipperToList { left, focus, right } = 
+    List.concat $ List.fromFoldable [List.reverse left, Cons focus right]
+
+--------------------------------------------------------------------------------
+
+data Tree a = Tree a (List (Tree a))
+
+treeValue :: forall a . Tree a -> a
+treeValue (Tree v _) = v
+
+treeChildren :: forall a . Tree a -> List (Tree a)
+treeChildren (Tree _ cs) = cs
+
+--------------------------------------------------------------------------------

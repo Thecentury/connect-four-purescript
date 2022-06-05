9a28d5ac66de0b1abc6567ebd193b5120c253909
Mikhail Brinchuk <thecentury@gmail.com>
2022-06-05 12:05:20 +0200

    Print new moves with a different color. Fixed boards comparison

--- src/GameLoop.purs
+++ src/GameLoop.purs
@@ -1,10 +1,11 @@
 module GameLoop where
 
-import ConnectFour (AIMove(..), Board, Config, Outcome(..), Player(..), boardOutcome, config, nextMove, nextPlayer, tryAddToBoard)
-import OwnPrelude (liftReader)
 import Prelude
 
+import Ansi.Codes (Color(..))
+import Ansi.Output (foreground, withGraphics)
 import BoardComparison (CellDiff(..), diffBoards)
+import ConnectFour (AIMove(..), Board, Config, Outcome(..), Player(..), boardOutcome, config, nextMove, nextPlayer, tryAddToBoard)
 import Control.Monad.Reader (ReaderT)
 import Data.Either (Either(..))
 import Data.Foldable (class Foldable, foldr)
@@ -20,6 +21,7 @@ import Node.Process as Process
 import Node.ReadLine (Interface)
 import Node.ReadLine as ReadLine
 import Node.Stream (writeString)
+import OwnPrelude (liftReader)
 
 question :: String -> Interface -> Aff String
 question message interface = makeAff go
@@ -61,8 +63,7 @@ putStrLn s = putStr (s <> "\n")
 
 drawCell :: CellDiff -> Aff Unit
 drawCell (Unchanged p) = putStr $ show p
--- todo colors
-drawCell (Changed p) = putStr $ show p
+drawCell (Changed p) = putStr $ withGraphics (foreground BrightRed) $ show p
 
 -- todo move me to prelude
 mapM_ :: forall a b t m. Monad m => Foldable t => (a -> m b) -> t a -> m Unit
@@ -98,22 +99,22 @@ loopWithDiff console player prevBoard board = do
     InProgress ->
       case playerKind player of
         AI -> do
-          board' <- nextMove player board
-          case board' of
-            Just (Definite board'') -> do
+          nextBoard <- nextMove player board
+          case nextBoard of
+            Just (Definite board') -> do
                 liftAff $ putStrLn "I'll win!"
-                loopWithDiff console (nextPlayer player) prevBoard board''
-            Just (RandomGuess board'') -> do
+                loopWithDiff console (nextPlayer player) board board'
+            Just (RandomGuess board') -> do
                 liftAff $ putStrLn "I hope I'll win..."
-                loopWithDiff console (nextPlayer player) prevBoard board''
+                loopWithDiff console (nextPlayer player) board board'
             Nothing -> liftAff $ putStrLn "AI failed to make the next move."
         Human -> do
             column <- readPlayerInput console player
-            let board' = tryAddToBoard player column board
-            case board' of
-                Just board'' -> do
+            let nextBoard = tryAddToBoard player column board
+            case nextBoard of
+                Just board' -> do
                     liftAff $ putStrLn ""
-                    loopWithDiff console (nextPlayer player) prevBoard board''
+                    loopWithDiff console (nextPlayer player) board board'
                 Nothing -> do
                     liftAff $ do
                         putStrLn $ "Column " <> show column <> " is full, choose another one."
